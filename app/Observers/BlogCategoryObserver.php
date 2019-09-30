6ae1fd6cbd841c0f15da4128c344534719248f10
Dari0n <smd26rus@yandex.ru>
2019-09-30 15:56:26 +0300

    BlogCategoryObserver

--- app/Observers/BlogCategoryObserver.php
+++ app/Observers/BlogCategoryObserver.php
@@ -7,6 +7,22 @@ use App\Models\BlogCategory;
 class BlogCategoryObserver
 {
     /**
+     * Если поле SLUG отсутствует, сгенерировать его из title категории перед обновлением записи
+     * @param BlogCategory $blogCategory
+     */
+    public function updating(BlogCategory $blogCategory)
+    {
+        $this->setSlug($blogCategory);
+    }
+    /**
+     * Если поле SLUG отсутствует, сгенерировать его из title категории перед созданием записи
+     * @param BlogCategory $blogCategory
+     */
+    public function creating(BlogCategory $blogCategory)
+    {
+        $this->setSlug($blogCategory);
+    }
+    /**
      * Handle the blog category "created" event.
      *
      * @param  \App\Models\BlogCategory  $blogCategory
@@ -59,5 +75,14 @@ class BlogCategoryObserver
     public function forceDeleted(BlogCategory $blogCategory)
     {
         //
+    }
+
+    protected function setSlug(BlogCategory $blogCategory)
+    {
+        if(empty($blogCategory->slug))
+        {
+            $blogCategory->slug = \Str::slug($blogCategory->title);
+
+        }
     }
 }
